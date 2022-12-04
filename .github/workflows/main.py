ea7bca647340d86adffc342ad5da18c03ce9a708
sreeshanth999 <107832530+sreeshanth999@users.noreply.github.com>
2022-12-04 08:39:05 +0530

    main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,5 @@
+hight= input('enter your current hight')
+if  int (hight)>5000 :
+     print('go arround and down your altitude less than 5000ft')
+elif int (hight)<=5000: 
+     print(' you are eligble to landing')
